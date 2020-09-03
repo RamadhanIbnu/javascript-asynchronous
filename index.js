812d08634658f73cb2afe206f981d2a3969004a8
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-09-03 14:02:08 +0700

    solve async problem

--- index.js
+++ index.js
@@ -3,34 +3,29 @@ const getNationality = async () => {
     try {
         let response = await fetch("https://restcountries.eu/rest/v2/all");
         let data = await response.json();
-        return myFunction(data);
+        data.forEach(function (getData) {
+            console.log(getData)
+                const div = document.createElement('div')
+                div.className = "col-lg-3 col-md-6 mb-4 data";
+            
+                div.innerHTML =
+                '<div class="card h-100">' +
+                    '<img class="card-img-top" src="' +getData.flag+ '" alt="fruit image">'+
+                        '<div class="card-body">' +
+                            '<h4 class="card-title">' +getData.name+ '</h4>' +
+                        '</div>'+
+                        '<div class="card-footer">'+
+                            '<a href="" class="btn btn-primary">Find Out More!</a>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'
+                document.getElementById("showData").appendChild(div);
+        });
     } catch (error) {
-        console.log("maaf yang anda cari tidak ditemukan")
+        console.log("maaf data yang anda cari tidak ditemukan")
     }
 }
 getNationality()
-// console.log(data)
-function myFunction(data){
-    data.forEach(function (getData) {
-        console.log(getdata)
-            div.className = "col-lg-3 col-md-6 mb-4 data";
-        
-            div.innerHTML =
-            '<div class="card h-100">' +
-                '<img class="card-img-top" src="' +getData.flag+ '" alt="fruit image">'+
-                    '<div class="card-body">' +
-                        '<h4 class="card-title">' +dataData.name+ '</h4>' +
-                    '</div>'+
-                    '<div class="card-footer">'+
-                        '<a href="" class="btn btn-primary">Find Out More!</a>'+
-                    '</div>'+
-                '</div>'+
-            '</div>'
-            document.getElementById("showData").appendChild(div);
-        });
-    
-}
-
     
     // fetch("https://restcountries.eu/rest/v2/all")
     // .then ((response)=>{
